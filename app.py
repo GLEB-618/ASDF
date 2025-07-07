2471cf7e5e2cc3c7fc4d2f79271a732b00f3ab23
RaykDrago <dragoposh1@gmail.com>
2025-07-07 21:23:21 +0300

    Сайт v0.1

--- app.py
+++ app.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route("/liking")
+def liking():
+    return render_template('liking.html')
+
+@app.route("/settings")
+def setting():
+    return render_template('settings.html')
+
+@app.route("/info")
+def info():
+    return render_template('info.html')
+
+@app.route("/help")
+def help():
+    return render_template('help.html')
+
+@app.route("/complaints")
+def complaints():
+    return render_template('complaints.html')
+
+@app.route("/channel")
+def channel():
+    return render_template('channel.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
